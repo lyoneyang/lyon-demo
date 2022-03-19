66a59a27665b9e263c3d4d6d7a28e5513c54a0e8
lyon <3437376487@qq.com>
2022-03-19 22:34:40 +0800

    Update ly_firstpy.py
    第二个版本

--- ly_firstpy.py
+++ ly_firstpy.py
@@ -5,3 +5,18 @@ for a in range(1,11):
         continue
     else:
         print(a)
+print('和平')
+b=1
+while b<40:
+    b=b+1
+    if b%4==0 or b%10==4 or b%4==10:
+        continue
+    else:
+        print(b)
+        str('12345')
+c=input('请输入你的名称：')
+d=int(input('请输入你的年龄：'))
+if c=='张珊' and d=='10':
+    print('您是个优秀且快乐的男生')
+else:
+    print('对不起，我还不认识你')
